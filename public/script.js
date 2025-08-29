4abe212c20fe0958e714a73056eddc4b3ce3d88c
NujabesDev <mwitk2@uic.edu>
2025-08-29 01:34:56 -0500

    confetti

--- public/script.js
+++ public/script.js
@@ -1,4 +1,14 @@
-document.addEventListener('click', function() {
-      const audio = new Audio('yippee.mp3');
-      audio.play();
-  });
+document.addEventListener('click', function(e) {
+    const audio = new Audio('yippee.mp3');
+    audio.play();
+
+    // Real confetti
+    confetti({
+        particleCount: 200,
+        spread: 150,
+        origin: {      
+            x: e.clientX / window.innerWidth,
+            y: e.clientY / window.innerHeight 
+        }
+    });
+});
